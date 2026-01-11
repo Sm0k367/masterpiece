3c4c1e2b1c3f58f1fb103782b64f13305d061896
Sm0k367 <139561920+Sm0k367@users.noreply.github.com>
2026-01-10 22:36:02 -0500

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="header-content">
+        <h1>Epic Tech AI</h1>
+        <p>The Embodied Will of Innovation</p>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
